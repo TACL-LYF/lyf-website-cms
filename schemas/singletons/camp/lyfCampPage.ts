c31f4fa2f6ccaaeef30b2ce9131d6687144283e1
Stephan Loh <sloh@fb.com>
2023-01-16 18:38:30 -0800

    Add header photos to what is camp

--- schemas/singletons/camp/lyfCampPage.ts
+++ schemas/singletons/camp/lyfCampPage.ts
@@ -16,6 +16,19 @@ const schema: DocumentDefinition = {
             title: "Sub Header",
             type: "text",
         },
+        {
+            name: "headerPhotos",
+            title: "Header Photos",
+            description:
+                "The set of photos to display in the header. Be sure to add alt text in the separate media browser!",
+            type: "array",
+            of: [
+                {
+                    type: "image",
+                },
+            ],
+        },
+
     ],
 }
 
