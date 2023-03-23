a7f2a98655d507f739ae203fe9c8cb7ce1340772
Stephan Loh <sloh4137@gmail.com>
2023-03-22 21:27:31 -0700

    Format dropdown

--- schemas/objects/dropdown.ts
+++ schemas/objects/dropdown.ts
@@ -1,21 +1,21 @@
 import { ObjectDefinition } from "sanity"
 
 const schema: ObjectDefinition = {
-  name: "dropdown",
-  title: "Dropdown",
-  type: "object",
-  fields: [
-      {
-          name: "dropdownHeader",
-          title: "Dropdown Header",
-          type: "string",
-      },
-      {
-          name: "dropdownBody",
-          title: "Contents of the Dropdown",
-          type: "portableText",
-      },
-  ],
+    name: "dropdown",
+    title: "Dropdown",
+    type: "object",
+    fields: [
+        {
+            name: "dropdownHeader",
+            title: "Dropdown Header",
+            type: "string",
+        },
+        {
+            name: "dropdownBody",
+            title: "Contents of the Dropdown",
+            type: "portableText",
+        },
+    ],
 }
 
 export default schema
