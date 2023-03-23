0f3daf461a33686ab9159fea5a26d752e7012de3
Stephan Loh <sloh4137@gmail.com>
2023-03-23 00:38:37 -0700

    Add slug to product

--- schemas/documents/product.ts
+++ schemas/documents/product.ts
@@ -1,5 +1,7 @@
 import { DocumentDefinition } from "sanity"
 
+// async function slugifyProduct
+
 const schema: DocumentDefinition = {
     name: "product",
     title: "Product",
@@ -12,20 +14,39 @@ const schema: DocumentDefinition = {
             validation: (Rule) => Rule.required(),
         },
         {
-            name: "subheader",
-            title: "Subheader",
-            type: "string",
-        },
+          name: "subheader",
+          title: "Subheader",
+          type: "string",
+      },
         {
             name: "edition",
             title: "Edition",
             type: "string",
         },
         {
+            name: "slug",
+            title: "Slug",
+            type: "slug",
+            description:
+                "This is the unique ID we'll use for the URL of the product. " +
+                "You can either generate it from the name and edition or input your own one.",
+            options: {
+                source: (doc, _context) => `${doc.name} ${doc.edition ?? ""}`,
+            },
+            validation: (Rule) => Rule.required(),
+        },
+
+        {
             name: "price",
             title: "Price",
             description: "Price in USD",
             type: "number",
+        },
+        {
+          name: "isDonation",
+          title: "Is Donation?",
+          description: "Whether we should mark this product as an appreciation gift for a donation.",
+          type: "boolean",
         },
         {
             name: "paymentLinkButton",
