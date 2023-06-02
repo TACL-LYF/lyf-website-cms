6ac575374291127dacbee59ab5e3bd3a2e9d70a0
Shialan Yu <shialan@ucsb.edu>
2023-06-02 16:41:54 -0700

    Add fields to donate page schema

--- schemas/singletons/get-involved/donatePage.ts
+++ schemas/singletons/get-involved/donatePage.ts
@@ -16,6 +16,32 @@ const schema: DocumentDefinition = {
             title: "Sub Header",
             type: "text",
         },
+        {
+            name: "headerImage",
+            title: "Header Image",
+            type: "image",
+        },
+        {
+            name: "headerButton",
+            title: "Header button",
+            description: "Button to 'Donate Now'",
+            type: "button",
+        },
+        {
+            name: "donationAmtHeader",
+            title: "Donation Amount Section Header",
+            type: "string",
+        },
+        {
+            name: "donationAmtSubHeader",
+            title: "Donation Amount Section SubHeader",
+            type: "string",
+        },
+        {
+            name: "donationAmtBody",
+            title: "Donation Amount Section Body",
+            type: "portableText",
+        },
     ],
 }
 
